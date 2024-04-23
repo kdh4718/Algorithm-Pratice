65fc61b91c9cf3dffae52893f4ba31c7fef45c8d
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-04-23 18:01:56 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String s = Integer.toString(n);
+        
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            answer += Character.getNumericValue(c);
+        }
+        
+
+        return answer;
+    }
+}
