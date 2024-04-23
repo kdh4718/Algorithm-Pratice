2e53fcdbe617511ee0cc905dc09eca0907e4d6be
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-04-23 17:57:50 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3856 ms, Memory: 151300 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+n, m = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(n)]
+
+visited = [[[False] * 2 for _ in range(m)] for _ in range(n)]
+visited[0][0][0] = True
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def bfs():
+    q = deque([(0, 0, 0, 1)])  
+    
+    while q:
+        x, y, wall_broken, dist = q.popleft()
+        
+        if x == n - 1 and y == m - 1:  
+            return dist
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == 1 and wall_broken == 0:  
+                    visited[nx][ny][1] = True  
+                    q.append((nx, ny, 1, dist + 1))
+                    
+                elif graph[nx][ny] == 0 and not visited[nx][ny][wall_broken]:
+                    visited[nx][ny][wall_broken] = True
+                    q.append((nx, ny, wall_broken, dist + 1))
+    return -1  
+
+print(bfs())
