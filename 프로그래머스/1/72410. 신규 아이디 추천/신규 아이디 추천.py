14b9555f3c26bfa49978e68b385a7e5086d99b2c
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-04-28 18:33:40 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.23 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,15 @@
+import re
+
+def solution(new_id):
+    answer = re.sub(r'[^\w\-_.]', '', new_id.lower())
+    answer = re.sub(r'\.{2,}', '.', answer)
+    answer = re.sub(r'^\.|\.$', '', answer)
+    answer = re.sub(r'\.$', '', answer[:15])
+    
+    if not answer:
+        answer = 'a'
+        
+    while len(answer) < 3:
+        answer += answer[-1]
+
+    return answer
