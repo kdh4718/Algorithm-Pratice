56abf4e433e0fd08bf790af6da355c23c2f83b6d
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-04-29 13:39:52 +0900

    [Bronze V] Title: ??!, Time: 200 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String id;
+		
+		Scanner sc = new Scanner(System.in);
+		id = sc.next();
+		id = id.concat("??!");
+		System.out.println(id);
+	}
+
+}
