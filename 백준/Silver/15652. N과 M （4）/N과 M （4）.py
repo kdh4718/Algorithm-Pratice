7b457e09e28abb7f876074fd04932209a1824168
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-04-29 13:45:37 +0900

    [Silver III] Title: N과 M (4), Time: 72 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,16 @@
+def back_sort():
+    if len(nmsort) == m:
+        print(*nmsort)
+        return
+    for i in range(1, n+1):
+        if not nmsort:
+            nmsort.append(i)
+            back_sort()
+            nmsort.pop()
+        elif i >= max(nmsort):
+            nmsort.append(i)
+            back_sort()
+            nmsort.pop()
+n, m = map(int, input().split())
+nmsort = []
+back_sort()
