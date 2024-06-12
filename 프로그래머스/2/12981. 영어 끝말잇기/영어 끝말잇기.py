2dd6862fa52d7ad56b7fc0cedadba387c6926ac2
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-06-12 23:00:41 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,5 +1,4 @@
 def solution(n, words):
-    answer = [2, 1]
     wordList = []
     
     for i in range(len(words)):
@@ -8,13 +7,8 @@ def solution(n, words):
             continue
         
         if words[i] in wordList or wordList[-1][-1] != words[i][0]:
-            return answer
-        
-        if answer[0] == n:
-            answer[0] = 1
-            answer[1] += 1
-        else:
-            answer[0] += 1
+            return [(i%n)+1, (i//n)+1]
+
         wordList.append(words[i])
 
     return [0, 0]
