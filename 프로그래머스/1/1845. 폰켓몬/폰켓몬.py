0d1bbce8189fe88cadf86fc6d9215239e6c6219e
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-06-14 14:55:39 +0900

    [level 1] Title: 폰켓몬, Time: 0.50 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,5 @@
 def solution(nums):
     answer = 0
     dic = {i: 1 for i in nums}
-            
-    if len(nums)//2 >= len(dic):
-        return len(dic)
-    else:
-        return len(nums)//2
-    
-    return answer
+
+    return min(len(nums)//2, len(dic))
