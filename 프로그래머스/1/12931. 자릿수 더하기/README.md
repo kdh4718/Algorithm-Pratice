ecfa824ec29af78321128cd58b80381b466ec8fa
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-07-06 09:24:11 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/README.md
+++ 프로그래머스/1/12931. 자릿수 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 자릿수 더하기 - 12931 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12931?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12931?language=java) 
 
 ### 성능 요약
 
-메모리: 60.3 MB, 시간: 7.32 ms
+메모리: 76.7 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 03일 17:37:44
+2024년 07월 06일 09:24:09
 
 ### 문제 설명
 
--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,14 +3,12 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        String s = Integer.toString(n);
         
-        for (int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            answer += Character.getNumericValue(c);
+        while(n > 0){
+            answer += n % 10;
+            n /= 10;
         }
         
-
         return answer;
     }
 }
