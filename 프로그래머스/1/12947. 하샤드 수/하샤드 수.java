d8ada30efd2e5843d3343fb1ed6771c783e410ab
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-07-08 08:06:52 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,6 +1,11 @@
 class Solution {
     public boolean solution(int x) {
-        int answer = String.valueOf(x).chars().map(i -> i - '0').sum();
+        int answer = 0;
+        int a = x;
+        while(a > 0){
+            answer += a % 10;
+            a /= 10;
+        }
         return x%answer == 0;
     }
 }
