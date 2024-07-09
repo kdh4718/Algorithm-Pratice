7218191d5f401cf70d510e72312ef7c203c418b0
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-07-09 20:27:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.96 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/README.md
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/README.md
@@ -1,10 +1,10 @@
 # [level 1] 수박수박수박수박수박수? - 12922 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12922?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12922) 
 
 ### 성능 요약
 
-메모리: 72.9 MB, 시간: 2.20 ms
+메모리: 80.2 MB, 시간: 1.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 18일 19:23:21
+2024년 07월 09일 20:27:51
 
 ### 문제 설명
 
--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,15 +1,6 @@
 class Solution {
     public String solution(int n) {
-        String answer = "";
-        
-        for (int i = 0; i < n; i++){
-            if (i % 2 == 0){
-                answer += "수";
-            }else{
-                answer += "박";
-            }
-        }
-        
-        return answer;
+        String answer = "수박".repeat(n/2);
+        return (n%2==0) ? answer : answer + "수";
     }
 }
