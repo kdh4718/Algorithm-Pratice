be64ac6e99947a86c5665cc47be9e06a61dfb118
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-08-07 09:03:56 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 114 ms, Memory: 44,804 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
@@ -1,6 +1,6 @@
 for test_case in range(1, 11):
     n = int(input())
-    tree = [[] for _ in range(n+1)]
+    tree = [""] * (n + 1)
     result = ""
 
     def inOrder(root):
@@ -8,14 +8,14 @@ for test_case in range(1, 11):
         if root > n:
             return
 
-        inOrder(root*2)
-        result += tree[root][0][0]
-        inOrder((root*2) + 1)
+        inOrder(root * 2)
+        result += tree[root]
+        inOrder((root * 2) + 1)
 
     for _ in range(n):
-        node = list(map(str, input().split()))
-        tree[int(node[0])].append(node[1:])
+        node = input().split()
+        tree[int(node[0])] = node[1]
 
     inOrder(1)
 
-    print(f"#{test_case}", result)
+    print(f"#{test_case} {result}")
