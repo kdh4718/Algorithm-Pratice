ba2878e6d839af87b576ba06d678a70b965c011a
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-08-22 11:57:56 +0900

    [Gold III] Title: 치즈, Time: 868 ms, Memory: 34136 KB -BaekjoonHub

---
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -0,0 +1,68 @@
+from collections import deque
+
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
+year = 0
+
+def bfs(i, j):
+    melt = []
+    q = deque()
+    q.append((i, j))
+    visited[i][j] = True
+
+    while q:
+        x, y = q.popleft()
+        count = 0
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == 1 and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
+                if graph[nx][ny] == 0 and visited[nx][ny]:
+                    count += 1
+
+        if count >= 2:
+            melt.append((x, y))
+
+    for x, y in melt:
+        graph[x][y] = 0
+
+def meltingPoint(i, j):
+    q = deque()
+    q.append((i, j))
+    visited[i][j] = True
+
+    while q:
+        x, y = q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+                if not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
+
+    return
+
+while True:
+    visited = [[False]*m for _ in range(n)]
+    currentCount = 0
+    meltingPoint(0, 0)
+    currentCount += sum([graph[i].count(1) for i in range(n)])
+
+    if currentCount == 0:
+        break
+
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1 and not visited[i][j]:
+                bfs(i, j)
+
+    year += 1
+
+print(year)
