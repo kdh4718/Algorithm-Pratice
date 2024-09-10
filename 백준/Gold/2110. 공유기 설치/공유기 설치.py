dcf5b1d398d299ef45140f46050b6264ae7899c5
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-09-10 15:45:25 +0900

    [Gold IV] Title: 공유기 설치, Time: 5120 ms, Memory: 38848 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,29 +1,23 @@
 n, c = map(int, input().split())
-house = []
-
-for i in range(n):
-    house.append(int(input()))
-house.sort()  
-
-start = 1
-end = house[-1] - house[0]
+home = [int(input()) for _ in range(n)]
+home.sort()
+start, end = 0, home[-1] - home[0]
 result = 0
 
 while start <= end:
-    mid = (start + end)//2
-    
-    loc = house[0]
-    count = 1
-    
-    for i in range(1, len(house)):
-        if house[i] >= loc + mid:
-            count += 1
-            loc = house[i]
-            
-    if count >= c:
+    mid = (start + end) // 2
+    loc = home[0]
+    cnt = 1
+
+    for i in range(1, len(home)):
+        if home[i] >= loc + mid:
+            cnt += 1
+            loc = home[i]
+
+    if cnt >= c:
         start = mid + 1
         result = mid
-    else:    
+    else:
         end = mid - 1
-        
+
 print(result)
