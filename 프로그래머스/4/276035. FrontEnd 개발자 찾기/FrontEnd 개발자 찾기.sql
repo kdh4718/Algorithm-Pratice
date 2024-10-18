500e22b7bed517c10446bb1aa00c5e7b35dd6a94
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-10-18 12:51:38 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,6 +1,7 @@
 -- 코드를 작성해주세요
-select distinct d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+select d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
 from SKILLCODES s, DEVELOPERS d
 where s.CODE & d.SKILL_CODE = s.CODE
     and s.CATEGORY = 'Front End'
+group by d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
 order by d.ID
