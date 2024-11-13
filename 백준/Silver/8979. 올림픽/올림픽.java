05fee44b3aaf0fe04949ea672d63588cb7ba74ec
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-11-13 09:11:14 +0900

    [Silver V] Title: 올림픽, Time: 124 ms, Memory: 14788 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -18,25 +18,35 @@ public class Main {
             int num = Integer.parseInt(st.nextToken());
 
             for (int j = 0; j < 3; j++) {
-                if (st.hasMoreTokens()) {
-                    medals[i][j] = Integer.parseInt(st.nextToken());
-                } else {
-                    System.out.println("Error: Insufficient data for country " + num);
-                    return;
-                }
+                medals[i][j] = Integer.parseInt(st.nextToken());
             }
             medals[i][3] = num;
         }
 
         Arrays.sort(medals, (a, b) -> {
-            if (b[0] != a[0]) return Integer.compare(b[0], a[0]); 
-            if (b[1] != a[1]) return Integer.compare(b[1], a[1]); 
-            return Integer.compare(b[2], a[2]); 
+            if (b[0] != a[0]) return Integer.compare(b[0], a[0]); // 금메달 내림차순
+            if (b[1] != a[1]) return Integer.compare(b[1], a[1]); // 은메달 내림차순
+            return Integer.compare(b[2], a[2]); // 동메달 내림차순
         });
 
+        int targetGold = 0, targetSilver = 0, targetBronze = 0;
+        int rank = 1;
+
+        // m번 국가의 메달 정보를 찾기
         for (int i = 0; i < n; i++) {
             if (medals[i][3] == m) {
-                System.out.println(i);
+                targetGold = medals[i][0];
+                targetSilver = medals[i][1];
+                targetBronze = medals[i][2];
+                rank = i + 1; // 1부터 시작하는 순위
+                break;
+            }
+        }
+
+        // m번 국가와 같은 메달 수를 가진 국가의 순위 찾기
+        for (int i = 0; i < n; i++) {
+            if (medals[i][0] == targetGold && medals[i][1] == targetSilver && medals[i][2] == targetBronze) {
+                System.out.println(i + 1); // 1부터 시작하는 순위
                 break;
             }
         }
