cc2013cf49e7db46d1d9cd69a4d6b968dbd3522a
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2024-12-08 22:17:31 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.51 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,34 +1,8 @@
-def find(line):
-    return not sum(line)
-
-def solution(wallpaper):
-    answer = []
-    graph = [[0 if i == '.' else 1 for i in list(wall)] for wall in wallpaper]
-    length = len(graph)
-    dir = [0, 0, length, len(graph[0])]
-    
-    for i in range(4):
-        num = 0
-        
-        while True:
-            if i == 0:
-                line = graph[num]
-                print(1, line)
-            elif i == 1:
-                line = [graph[i][num] for i in range(length)]
-                print(2, line)
-            elif i == 2:
-                line = graph[length - num - 1]
-                print(3, line)
-            else:
-                line = [graph[i][len(graph[0]) - num - 1] for i in range(length)]
-                print(4, line)
-                
-            if not find(line): break
-            num += 1
-        
-        num = abs(dir[i] - num)
-        
-        answer.append(num)
-    
-    return answer
+def solution(wall):
+    a, b = [], []
+    for i in range(len(wall)):
+        for j in range(len(wall[i])):
+            if wall[i][j] == "#":
+                a.append(i)
+                b.append(j)
+    return [min(a), min(b), max(a) + 1, max(b) + 1]
