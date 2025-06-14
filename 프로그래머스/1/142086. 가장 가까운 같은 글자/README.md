466edeb498dad3ba1c48a761e606fd96c272587c
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2025-06-14 17:42:31 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.41 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/README.md
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 가까운 같은 글자 - 142086 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086) 
 
 ### 성능 요약
 
-메모리: 75.8 MB, 시간: 4.36 ms
+메모리: 89 MB, 시간: 3.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 20일 12:21:04
+2025년 06월 14일 17:42:28
 
 ### 문제 설명
 
--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -3,8 +3,8 @@ import java.util.*;
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
-        HashMap<Character,Integer> map = new HashMap<>();
-        
+        HashMap<Character, Integer> map = new HashMap<>();
+            
         for(int i=0; i<s.length();i++){
             char ch = s.charAt(i);
             answer[i] = i-map.getOrDefault(ch,i+1);
