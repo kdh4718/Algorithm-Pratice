521cf7c513d15ad6b1474e6dfc0ddc13add48799
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2025-06-15 09:43:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 77.44 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 98.2 MB, 시간: 96.17 ms
+메모리: 112 MB, 시간: 77.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 22일 15:26:53
+2025년 06월 15일 09:43:48
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,26 +2,23 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> map = new HashMap<>();
         String answer = "";
-
-        // 참가자 명단을 map에 추가
-        for (String p : participant) {
-            map.put(p, map.getOrDefault(p, 0) + 1);
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String part : participant){
+            map.put(part, map.getOrDefault(part, 0) + 1);
         }
-
-        // 완주자 명단을 기반으로 카운트 감소
-        for (String c : completion) {
-            map.put(c, map.get(c) - 1);
+        
+        for (String comp : completion){
+            map.put(comp, map.get(comp) - 1);
         }
-
-        // 카운트가 1인 사람이 완주하지 못한 사람
-        for (String key : map.keySet()) {
-            if (map.get(key) != 0) {
-                answer = key;
+        
+        for (String part : map.keySet()){
+            if (map.get(part) != 0){
+                answer = part;
             }
         }
-
-        return answer; 
+        
+        return answer;
     }
 }
