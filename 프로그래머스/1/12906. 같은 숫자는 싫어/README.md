3a25fb2ba3b75608edac1379b75ece2e5a191a8e
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2025-06-15 10:33:07 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 26.68 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 119 MB, 시간: 35.25 ms
+메모리: 111 MB, 시간: 26.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 22일 17:37:59
+2025년 06월 15일 10:33:05
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,19 +2,23 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        List<Integer> tempList = new ArrayList<>();
-
-        for (int a : arr) {
-            if (tempList.size() == 0 || tempList.get(tempList.size() - 1) != a)             {
-                tempList.add(a);
+        int[] answer;
+        List<Integer> stack = new ArrayList<>();
+        int pre = -1;
+        
+        for (int num : arr){
+            if (num != pre){
+                stack.add(num);
             }
-        }
-
-        int[] answer = new int[tempList.size()];
-        for (int i = 0; i < tempList.size(); i++) {
-            answer[i] = tempList.get(i);
+            pre = num;
         }
         
+        answer = new int[stack.size()];
+        
+        for (int i = 0; i < stack.size(); i++){
+            answer[i] = stack.get(i);
+        }
+
         return answer;
     }
 }
