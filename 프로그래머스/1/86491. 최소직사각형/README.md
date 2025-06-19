e8bdcba4e2817b188cecea62802191605e420151
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2025-06-19 16:24:20 +0900

    [level 1] Title: 최소직사각형, Time: 2.55 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
 
 ### 성능 요약
 
-메모리: 88.7 MB, 시간: 1.40 ms
+메모리: 76.5 MB, 시간: 2.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 15일 07:47:42
+2025년 06월 19일 16:24:15
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,13 +1,15 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int maxWidth = 0;
-        int maxHeight = 0;
+        int answer;
+        int row = 0;
+        int col = 0;
         
-        for (int[] size: sizes){
-            maxWidth = Math.max(maxWidth, Math.max(size[0], size[1]));
-            maxHeight = Math.max(maxHeight, Math.min(size[0], size[1]));
+        for (int[] size : sizes){
+            row = Math.max(row, Math.min(size[0], size[1]));
+            col = Math.max(col, Math.max(size[0], size[1]));
         }
+        answer = row * col;
         
-        return maxWidth*maxHeight;
+        return answer;
     }
 }
