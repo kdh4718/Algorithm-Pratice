b511a3060e7f81af832684c57aef91c27021da62
kdh4718 <103204748+kdh4718@users.noreply.github.com>
2025-08-07 13:25:28 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.56 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = my_string.toLowerCase();
+        
+        char[] ch = answer.toCharArray();
+        Arrays.sort(ch);
+        
+        return new String (ch);
+    }
+}
